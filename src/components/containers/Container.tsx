bf01f35b5ea4bab8e92fbb76292333785445f259
jpricardo <joaopds.ricardo@gmail.com>
2024-07-20 19:15:38 -0300

    feat: basic containers

---
+++ src/components/containers/Container.tsx
@@ -0,0 +1,48 @@
+import { Flex } from 'antd';
+import { memo } from 'react';
+import styled from 'styled-components';
+
+const StyledContainer = styled.div<{ $hover?: boolean }>`
+	backgroundcolor: #fefefe;
+	border: 1px solid #c3c3c3;
+	padding: 1rem;
+	border-radius: 0.25rem;
+
+	cursor: ${({ $hover }) => ($hover ? 'pointer' : 'initial')};
+
+	&:hover {
+		box-shadow: ${({ $hover }) => ($hover ? 'rgba(0, 0, 0, 0.25) 0px 0px 8px 0px' : '')};
+	}
+`;
+
+type ContainerProps = React.HtmlHTMLAttributes<HTMLDivElement> & {
+	title?: React.ReactNode;
+	addon?: React.ReactNode;
+	hover?: boolean;
+};
+function Container({ title, addon, children, hover, ...props }: ContainerProps) {
+	return (
+		<StyledContainer $hover={hover} {...props}>
+			{!!title && (
+				<Flex
+					justify='space-between'
+					align='center'
+					style={{
+						borderBottom: '1px solid #c3c3c3',
+						padding: '1rem',
+						margin: '-1rem -1rem 1rem',
+						backgroundColor: '#f0f0f0',
+					}}
+				>
+					<span style={{ fontSize: '1rem', fontWeight: 500 }}>{title}</span>
+
+					{addon}
+				</Flex>
+			)}
+
+			{children}
+		</StyledContainer>
+	);
+}
+
+export default memo(Container);
---
+++ src/components/containers/ControlCard.tsx
@@ -0,0 +1,25 @@
+import { Flex } from 'antd';
+import styled from 'styled-components';
+
+import Title from '../typography/Title';
+
+const StyledControlCard = styled.div`
+	padding: 0.25rem;
+`;
+
+type ControlCardProps = React.HtmlHTMLAttributes<HTMLDivElement> & {
+	title: React.ReactNode;
+};
+function ControlCard({ title, children, ...props }: ControlCardProps) {
+	return (
+		<StyledControlCard {...props}>
+			<Flex gap='0.5rem' vertical>
+				<Title>{title}</Title>
+
+				<div>{children}</div>
+			</Flex>
+		</StyledControlCard>
+	);
+}
+
+export default ControlCard;

8b21d961a49f3be546e7cf1954b549400ce8e572
jpricardo <joaopds.ricardo@gmail.com>
2024-07-20 19:12:47 -0300

    feat: helpers and global types

---
+++ src/lib/index.ts
@@ -0,0 +1,31 @@
+export type JobType = 'FullTime' | 'Hybrid' | 'Remote';
+
+export type OrderByType = 'Most relevant' | 'Most recent' | 'Best pay';
+
+export type AreaType = 'Development' | 'Design' | 'DevOps' | 'QA' | 'Management';
+
+export type Job = {
+	id: number;
+	title: string;
+	company: string;
+	description: string;
+
+	yearlySalary: number;
+	createdAt: Date;
+
+	jobType: JobType;
+	areaType: AreaType;
+	tags: string[];
+};
+
+export const getRandomNumber = (min = 0, max = 1) => min + Math.floor(Math.random() * (max - min));
+
+export const getDateDifferenceInDays = (start: Date, end: Date) => {
+	const msInDay = 1000 * 3600 * 24;
+
+	const diff = end.getTime() - start.getTime();
+
+	return Math.floor(diff / msInDay);
+};
+
+export const timer = (ms: number) => new Promise<void>((resolve) => setTimeout(resolve, ms));

f3d4c4d53a270fe08083495903b2a18145e635cc
jpricardo <joaopds.ricardo@gmail.com>
2024-07-21 18:16:02 -0300

    feat: Job type update

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,10 +4,13 @@ export type OrderByType = 'Most relevant' | 'Most recent' | 'Best pay';
 
 export type AreaType = 'Development' | 'Design' | 'DevOps' | 'QA' | 'Management';
 
+export type SeniorityLevelType = 'Junior' | 'Associate' | 'Senior' | 'Lead';
+
 export type Job = {
 	id: number;
 	title: string;
 	company: string;
+	shortDescription: string;
 	description: string;
 
 	yearlySalary: number;
@@ -15,6 +18,8 @@ export type Job = {
 
 	jobType: JobType;
 	areaType: AreaType;
+	seniorityLevel: SeniorityLevelType;
+
 	tags: string[];
 };
 

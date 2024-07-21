abb7fc52f4ab3d71a7d68b9c50f60857957c0ea5
jpricardo <joaopds.ricardo@gmail.com>
2024-07-21 18:21:15 -0300

    feat: responsive padding for the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 import { Flex } from 'antd';
 import { memo } from 'react';
 
+import { useIsMobile } from '@/hooks/useIsMobile';
+
 import Headline from './typography/Headline';
 
 function Header() {
+	const isMobile = useIsMobile();
+
 	return (
-		<Flex justify='center' align='center' style={{ margin: '2rem' }}>
+		<Flex justify='center' align='center' style={{ margin: isMobile ? '1rem' : '1.5rem' }}>
 			<Headline>Job Listing App</Headline>
 		</Flex>
 	);

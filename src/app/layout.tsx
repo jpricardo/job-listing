547616d38d16b84b4a943906f32c53bd8233b0e6
jpricardo <joaopds.ricardo@gmail.com>
2024-07-21 20:29:41 -0300

    feat: analytics!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { memo } from 'react';
@@ -19,7 +20,11 @@ function RootLayout({ children }: RootLayoutProps) {
 	return (
 		<html lang='en'>
 			<body className={inter.className}>
-				<Providers>{children}</Providers>
+				<Providers>
+					<Analytics />
+
+					{children}
+				</Providers>
 			</body>
 		</html>
 	);

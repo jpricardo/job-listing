f4363ae1967e94023ab0495e213398728b0f8e44
jpricardo <joaopds.ricardo@gmail.com>
2024-07-21 19:18:33 -0300

    feat: new methods for getting random data out of arrays

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -25,6 +25,21 @@ export type Job = {
 
 export const getRandomNumber = (min = 0, max = 1) => min + Math.floor(Math.random() * (max - min));
 
+export const getRandomItem = <TData>(arr: TData[]) => arr[getRandomNumber(0, arr.length)];
+
+export const getRandomSample = <TData>(arr: TData[], sampleSize = 1) => {
+	let validPicks: TData[] = [...arr];
+	const picks: TData[] = [];
+
+	for (let i = 0; i < sampleSize; i++) {
+		const newItem = getRandomItem(validPicks);
+		validPicks = validPicks.filter((item) => item !== newItem);
+		picks.push(newItem);
+	}
+
+	return picks;
+};
+
 export const getDateDifferenceInDays = (start: Date, end: Date) => {
 	const msInDay = 1000 * 3600 * 24;
 

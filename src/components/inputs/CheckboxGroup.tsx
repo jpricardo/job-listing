9aa5665d0fe4fcf68ce168b144b51788df45eff3
jpricardo <joaopds.ricardo@gmail.com>
2024-07-21 18:23:11 -0300

    feat: controllable CheckboxGroup component

--- src/components/inputs/CheckboxGroup.tsx
+++ src/components/inputs/CheckboxGroup.tsx
@@ -4,21 +4,40 @@ import { memo } from 'react';
 import Label from '../typography/Label';
 
 type OptionType = string | number;
-type CheckboxGroupProps = {
-	options: OptionType[];
 
+type CheckboxGroupProps = {
+	name: string;
+
+	options: OptionType[];
 	value?: OptionType[];
 	onChange?: (value: OptionType[]) => void;
 };
 
-function CheckboxGroup({ options, value, onChange }: CheckboxGroupProps) {
+function CheckboxGroup({ name, options, value, onChange }: CheckboxGroupProps) {
+	const addOptionToValue = (option: OptionType, value?: OptionType[]) => {
+		return [...(value || []), option];
+	};
+
+	const removeOptionFromValue = (option: OptionType, value?: OptionType[]) => {
+		return value?.filter((item) => option !== item) || [];
+	};
+
 	return (
 		<Flex vertical>
 			{options.map((option, index) => {
-				const checkboxName = `checkbox-item-${index}`;
+				const checkboxName = `${name}-checkbox-item-${index}`;
 				return (
 					<Flex key={index} gap='0.25rem' align='center'>
-						<input type='checkbox' id={checkboxName} name={checkboxName} />
+						<input
+							type='checkbox'
+							id={checkboxName}
+							name={checkboxName}
+							checked={value?.includes(option)}
+							onChange={(e) => {
+								if (e.target.checked) return onChange?.(addOptionToValue(option, value));
+								return onChange?.(removeOptionFromValue(option, value));
+							}}
+						/>
 						<Label htmlFor={checkboxName}>{option}</Label>
 					</Flex>
 				);

8aa57f82b8e126030ba108b47e4361f2e849e74f
jpricardo <joaopds.ricardo@gmail.com>
2024-07-21 21:18:59 -0300

    fix: small adjustment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
 	const isMobile = useIsMobile();
 
 	return (
-		<Flex justify='center' align='center' style={{ margin: isMobile ? '1rem' : '1.5rem' }}>
+		<Flex justify='center' align='center' style={{ margin: isMobile ? '0.5rem' : '1.5rem' }}>
 			<Headline>Job Listing App</Headline>
 		</Flex>
 	);

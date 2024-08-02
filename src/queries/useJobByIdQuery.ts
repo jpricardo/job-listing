f2389002598c11d6642655f47c274ba8a41bc548
jpricardo <joaopds.ricardo@gmail.com>
2024-08-01 21:34:30 -0300

    fix: undefined  query result bug fix

--- src/queries/useJobByIdQuery.ts
+++ src/queries/useJobByIdQuery.ts
@@ -4,5 +4,9 @@ import JobService from '@/services/JobService';
 
 export default function useJobByIdQuery(id?: number) {
 	const jobService = new JobService();
-	return useQuery({ queryKey: ['job', id], queryFn: () => jobService.getById(id) });
+	return useQuery({
+		queryKey: ['job', id],
+		queryFn: () => jobService.getById(id),
+		enabled: typeof id === 'number',
+	});
 }

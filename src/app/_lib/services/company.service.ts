4a9b33d744a3c6fb4301e5035b8077837a7d12e2
jpricardo <joaopds.ricardo@gmail.com>
2025-06-09 23:07:18 -0300

    feat: new method, mock data

--- src/app/_lib/services/company.service.ts
+++ src/app/_lib/services/company.service.ts
@@ -1,4 +1,5 @@
 import { Company } from '../models/company.model';
+import { IDType } from '../types';
 import Service from './service';
 
 const companies = [
@@ -15,10 +16,21 @@ const companies = [
 		description: 'It just works',
 		tags: ['B2C', 'Big Tech', 'Remote'],
 	},
+
+	{
+		id: 3,
+		name: 'Company 03',
+		description: 'Yeah...',
+		tags: ['MVP', 'Fintech', 'Remote', 'Mobile'],
+	},
 ];
 
 export default class CompanyService extends Service {
 	public async getAll(): Promise<Company[]> {
 		return this.request(companies);
+	}
+
+	public async getById(id: IDType): Promise<Company | undefined> {
+		return this.request(companies.find((company) => company.id.toString() === id.toString()));
 	}
 }

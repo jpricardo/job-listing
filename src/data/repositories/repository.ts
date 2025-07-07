5118a7e085d4f2fa7f335bb433fbb7632eb786c6
jpricardo <joaopds.ricardo@gmail.com>
2025-07-07 16:11:41 -0300

    feat: interface polymorphism

--- src/data/repositories/repository.ts
+++ src/data/repositories/repository.ts
@@ -1,6 +1,6 @@
 import 'server-only';
 
-export interface IRepository<T> {
-	find(filters?: Partial<T>): Promise<T | undefined>;
-	findAll(filters?: Partial<T>): Promise<T[]>;
+export interface IRepository<T extends object | string | number> {
+	find: T extends object ? (filters?: Partial<T>) => Promise<T | undefined> : (item?: T) => Promise<T | undefined>;
+	findAll: T extends object ? (filters?: Partial<T>) => Promise<T[]> : (items?: T[]) => Promise<T[]>;
 }
